c6ca522ce9255d8360dff829c62e7de7032f1ad4
sadoru <sadoroo@gmail.com>
2015-06-10 08:39:30 +0900

    角丸ボタンにするサンプル
    Storyboard + UIButtonのカスタムクラスで、Storyboard上からの角丸ボタンをライブプレビュー可能に。
    Objective-C版

---
+++ ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  RoundedButton
+//
+//  Created by sadoru on 2015/06/09.
+//  Copyright (c) 2015年 sadoru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+
+@end
+
---
+++ ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  RoundedButton
+//
+//  Created by sadoru on 2015/06/09.
+//  Copyright (c) 2015年 sadoru. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
